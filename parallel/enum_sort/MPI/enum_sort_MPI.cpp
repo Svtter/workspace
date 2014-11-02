1333637687695625610d9b28910d424f638e891c
svtter <svtter@qq.com>
2014-11-03 07:56:19 +0800

    MPI

--- parallel/enum_sort/MPI/enum_sort_MPI.cpp
+++ parallel/enum_sort/MPI/enum_sort_MPI.cpp
@@ -17,12 +17,13 @@
 #include <sys/time.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 #include <iostream>
 
 using namespace std;
 
-#define MAXN 30000
-#define PMAX 10000
+#define MAXN 100
+#define PMAX 1000
 
 void build(int a[], int b[])
 {
@@ -59,39 +60,78 @@ void debug(int a[], int len)
 }
 
 int a[MAXN+10], b[MAXN+10], at[MAXN+10], bt[MAXN+10];
-int main(int argc, char **argv)
+
+void sort(int con[], int pos[], int &myid, int &numprocs)
+{
+    int i, j, k;
+    int no = 0;
+    for(i = myid; i <= MAXN; i+=numprocs)
+    {
+        k = 1;
+        for(j = 1; j <= MAXN; j++)
+        {
+            if(b[i] > b[j] || (b[i] == b[j] && (i > j)))
+                k++;
+        }
+        pos[no] = k;
+        con[no] = i;
+        no++;
+    }
+}
+
+int main(int argc, char *argv[])
 {
     int myid, numprocs;
     int namelen;
     char processor_name[MPI_MAX_PROCESSOR_NAME];
     double c1, c2;
     double start, end;
+    int i;
 
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &myid);
     MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
     MPI_Get_processor_name(processor_name, &namelen); 
-    fprintf(stderr, "Hello world! Processor %d of %d on %s\n", myid, numprocs, processor_name);
+    // fprintf(stderr, "Hello world! Processor %d of %d on %s\n", myid, numprocs, processor_name);
 
     //serial
     if(myid == 0)
     {
-        build(a, b);
+        build(a, b); 
         c1 = serial_enum_sort(a, at);
+        // debug(a, 100);
+        debug(at, 100);
         cout << "serial cost time is: " << c1 << endl;
     }
 
+    int con[MAXN+10];
+    int pos[MAXN+10];
+    memset(con, 0, sizeof(con));
+    memset(pos, 0, sizeof(pos));
+    memset(rank, 0, sizeof(rank));
+    start = MPI_Wtime();
     // P0 send b to ALL
     MPI_Bcast(b, MAXN+10, MPI_INT, 0, MPI_COMM_WORLD);
+    sort(con, pos, myid, numprocs);
 
     // Gather
-    if(myid == 0)
-    {
-        
-    }
+    // gather have problem: how can it 
+    MPI_Gather(con, MAXN/4, MPI_INT, con, MAXN/4, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Gather(pos, MAXN/4, MPI_INT, pos, MAXN/4, MPI_INT, 0, MPI_COMM_WORLD);
 
     if(myid == 0)
-        cout << "加速比为： " << c1 / c2 << endl;;
+    {
+        for(i = 0; i < MAXN; i++)
+        {
+            bt[con[i]] = b[pos[i]];
+        }
+        debug(bt, 100);
+        end = MPI_Wtime();
+        c2 = end - start;
+        // debug(bt, 100);
+        fprintf(stderr, "parallel cost time is: %lf\n", c2);
+        fprintf(stderr, "加速比为： %lf", c1 / c2);
+    }
 
     MPI_Finalize();
     return 0;
